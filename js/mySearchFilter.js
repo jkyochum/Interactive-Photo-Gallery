241845f81c602c132fe1e8ca60f5f8ff9c38744c
Jeremy Yochum <jyochum391@yahoo.com>
2023-02-16 21:59:50 -0500

    updating search filter

--- js/mySearchFilter.js
+++ js/mySearchFilter.js
@@ -1,6 +1,18 @@
 
+function getImage() {
+    let imgArr = [];
+    let image = document.querySelector('img');
+
+    for (i = 0; i < 12; i++) {
+        imgArr[i] = image;
+        imgArr[i].id = `'${i + 1}'`;
+    }
+    console.log(imgArr)
+}
+
 function search() {
     let userSearch = document.getElementById('search').value;
-    console.log(userSearch.toLowerCase());
+    userSearch = userSearch.toLowerCase();
+    console.log(x);
 }
 
